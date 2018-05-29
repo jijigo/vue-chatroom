009ad4b4230a54fd8f856055471bf6543063ae6b
debbyji <jasmine820921@gmail.com>
2018-05-29 23:21:03 +0800

    change hostname

--- index.js
+++ index.js
@@ -43,8 +43,8 @@
 // });
 
 const http = require('http');
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 5000;
+const hostname = '0.0.0.0';
+const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
