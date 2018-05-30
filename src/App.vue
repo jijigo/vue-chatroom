03e83db93cb007d8acb10820e7dd2b7f2bcd84b8
debbyji <jasmine820921@gmail.com>
2018-05-31 00:46:27 +0800

    try new io

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
 
 <script>
 import io from "socket.io-client";
-const socket = io("https://0.0.0.0:4040");
+const socket = io("https://vuechatroom.herokuapp.com:4040");
 
 export default {
   name: "app",
